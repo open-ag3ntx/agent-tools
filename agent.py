f322b406245eadb0cbb139974a2d857af57933d6
korade-krushna <krishnacodes15@gmail.com>
2025-12-07 20:58:33 +0530

    feat(agent): Choose Better Model

--- agent.py
+++ agent.py
@@ -23,7 +23,7 @@ from bash.tools import glob
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     temperature=0.1,
     api_key=os.getenv("GOOGLE_API_KEY")
 )
